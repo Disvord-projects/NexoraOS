188b755d4a118dd932e88deed571a12ba17a40ae
Disvord Projects <romanchiknep12345@outlook.com>
2025-04-26 23:53:20 +0700

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -2,13 +2,11 @@ GREEN='\033[32m'
 NC='\033[0m'
 
 LOGO='
-██╗  ██╗ ██╗ ██████╗       ██████╗ ██████╗  ██████╗ ███████╗
-╚██╗██╔╝███║██╔════╝       ██╔══██╗██╔══██╗██╔═══██╗██╔════╝
- ╚███╔╝ ╚██║███████╗ █████╗██████╔╝██████╔╝██║   ██║███████╗
- ██╔██╗  ██║██╔═══██╗╚════╝██╔═══╝ ██╔══██╗██║   ██║╚════██║
-██╔╝ ██╗ ██║╚██████╔╝      ██║     ██║  ██║╚██████╔╝███████║
-╚═╝  ╚═╝ ╚═╝ ╚═════╝       ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚══════╝
-____________________________________________________________
+  _   _                             ___  ____  
+ | \ | | _____  _____  _ __ __ _   / _ \/ ___| 
+ |  \| |/ _ \ \/ / _ \| '__/ _` | | | | \___ \ 
+ | |\  |  __/>  < (_) | | | (_| | | |_| |___) |
+ |_| \_|\___/_/\_\___/|_|  \__,_|  \___/|____/ 
 '
 
 echo -e "${GREEN}Compiling the bootloader${NC}"
@@ -17,8 +15,6 @@ nasm -f bin src/boot.asm -o bin/boot.bin
 echo -e "${GREEN}Compiling the kernel and programs${NC}"
 nasm -f bin src/kernel.asm -o bin/kernel.bin
 nasm -f bin src/write.asm -o bin/write.bin
-nasm -f bin src/brainf.asm -o bin/brainf.bin
-nasm -f bin src/barchart.asm -o bin/barchart.bin
 nasm -f bin src/snake.asm -o bin/snake.bin
 nasm -f bin src/calc.asm -o bin/calc.bin
 
@@ -28,8 +24,6 @@ dd if=/dev/zero of=disk_img/x16pros.img bs=512 count=25
 dd if=bin/boot.bin of=disk_img/x16pros.img conv=notrunc
 dd if=bin/kernel.bin of=disk_img/x16pros.img bs=512 seek=1 conv=notrunc
 dd if=bin/write.bin of=disk_img/x16pros.img bs=512 seek=8 conv=notrunc 
-dd if=bin/brainf.bin of=disk_img/x16pros.img bs=512 seek=11 conv=notrunc 
-dd if=bin/barchart.bin of=disk_img/x16pros.img bs=512 seek=14 conv=notrunc
 dd if=bin/snake.bin of=disk_img/x16pros.img bs=512 seek=15 conv=notrunc
 dd if=bin/calc.bin of=disk_img/x16pros.img bs=512 seek=17 conv=notrunc
 echo -e "${GREEN}Done.${NC}"
