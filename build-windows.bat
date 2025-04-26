d1aeebf5794828497b43b290ac5a38790ac32fb8
Disvord Projects <romanchiknep12345@outlook.com>
2025-04-26 23:50:15 +0700

    Update build-windows.bat

--- build-windows.bat
+++ build-windows.bat
@@ -28,8 +28,6 @@ echo Compiling the kernel and programs...
 nasm -f bin src/kernel.asm -o bin/kernel.bin
 nasm -f bin src/clock.asm -o bin/clock.bin
 nasm -f bin src/write.asm -o bin/write.bin
-nasm -f bin src/brainf.asm -o bin/brainf.bin
-nasm -f bin src/barchart.asm -o bin/barchart.bin
 
 echo Creating a disk image...
 fsutil file createnew disk_img/x16pros.img 12800
@@ -39,9 +37,6 @@ copy /b bin\boot.bin disk_img\x16pros.img
 powershell -Command "$bytes = [System.IO.File]::ReadAllBytes('bin\kernel.bin'); $stream = [System.IO.File]::OpenWrite('disk_img\x16pros.img'); $stream.Position = 512; $stream.Write($bytes, 0, $bytes.Length); $stream.Close()"
 powershell -Command "$bytes = [System.IO.File]::ReadAllBytes('bin\clock.bin'); $stream = [System.IO.File]::OpenWrite('disk_img\x16pros.img'); $stream.Position = 512 * 7; $stream.Write($bytes, 0, $bytes.Length); $stream.Close()"
 powershell -Command "$bytes = [System.IO.File]::ReadAllBytes('bin\write.bin'); $stream = [System.IO.File]::OpenWrite('disk_img\x16pros.img'); $stream.Position = 512 * 8; $stream.Write($bytes, 0, $bytes.Length); $stream.Close()"
-powershell -Command "$bytes = [System.IO.File]::ReadAllBytes('bin\brainf.bin'); $stream = [System.IO.File]::OpenWrite('disk_img\x16pros.img'); $stream.Position = 512 * 11; $stream.Write($bytes, 0, $bytes.Length); $stream.Close()"
-powershell -Command "$bytes = [System.IO.File]::ReadAllBytes('bin\barchart.bin'); $stream = [System.IO.File]::OpenWrite('disk_img\x16pros.img'); $stream.Position = 512 * 14; $stream.Write($bytes, 0, $bytes.Length); $stream.Close()"
-
 echo Starting the emulator...
 qemu-system-i386 -hda disk_img/x16pros.img
 
